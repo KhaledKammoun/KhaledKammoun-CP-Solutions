4e1a3d858cc6636671996bd7d2ea069c9994283c
KhaledKammoun <khaledsfax03@gmail.com>
2025-12-26 16:01:10 +0100

    Add solutions for Codeforces daily problems

---
+++ CodeForces/Codeforces Round 875 (Div. 2)/B. Array merging_v_1.cpp
@@ -0,0 +1,115 @@
+// ----------------- Competitive Programming Template -----------------
+// Author: Khaled Kammoun
+// GitHub: https://github.com/KhaledKammoun
+// LinkedIn: https://www.linkedin.com/in/khaled-kammoun-6b1059308/
+// #FREE_PALESTINE
+// #STOP_GENOCIDE_IN_GAZA
+// -------------------------------------------------------------------
+// Date: 2025-12-26
+// -------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define endl '\n'
+#define MOD 1000000007LL
+
+// ----------------- IO & VECTOR MACROS -----------------
+#define read_vector(v,n) for (int i = 0; i < n; i++) cin >> v[i];
+#define print_vector(v) for (size_t i = 0; i < v.size(); i++) { cout << v[i] << ' '; } cout << endl;
+#define print_vector_pair(v) for (size_t i = 0; i < v.size(); i++) { cout << v[i].first << " " << v[i].second << endl; }
+#define print_map_int(m) for (map<int,int>::iterator it = (m).begin(); it != (m).end(); ++it) cout << it->first << ": " << it->second << endl;
+#define print_map_ll(m) for (map<long long,long long>::iterator it = (m).begin(); it != (m).end(); ++it) cout << it->first << ": " << it->second << endl;
+
+// ----------------- FOR LOOPS -----------------
+#define f(i,x,n) for (int i = x; i < n; i++)
+#define f0(i,n) for (int i = 0; i < n; i++)
+#define f1(i,n) for (int i = 1; i < n; i++)
+
+// ----------------- MAP ITERATORS -----------------
+#define fmap_int(k,v,m) for (map<int,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_ll(k,v,m) for (map<long long,long long>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_char(k,v,m) for (map<char,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_string(k,v,m) for (map<string,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_pair(k,v,m) for (map<pair<int,int>,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+
+// ----------------- SET ITERATORS -----------------
+#define fmap_set(s,m) for (set<int>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+#define fmap_set_ll(s,m) for (set<long long>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+#define fmap_set_char(s,m) for (set<char>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+#define fmap_set_string(s,m) for (set<string>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+
+// ----------------- VECTOR ITERATORS -----------------
+#define fvec_int(x,v) for (vector<int>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+#define fvec_ll(x,v) for (vector<long long>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+#define fvec_char(x,v) for (vector<char>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+#define fvec_string(x,v) for (vector<string>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+
+void solve() {
+    int n; cin >> n;
+    vector<int> arr_a(n);
+    vector<int> arr_b(n);
+    read_vector(arr_a,n);
+    read_vector(arr_b,n);
+
+    unordered_map<int,int> count_a_map;
+    unordered_map<int,int> count_b_map;
+
+    unordered_set<int> unique_elements;
+
+    int longest_a = 1;
+    int longest_b = 1;
+
+    count_a_map[arr_a[0]] = 1;
+    count_b_map[arr_b[0]] = 1;
+    unique_elements.insert(arr_a[0]);
+    unique_elements.insert(arr_b[0]);
+
+    f1(i,n) {
+        if (arr_a[i] == arr_a[i-1]) {
+            longest_a++;
+        } else {
+            longest_a = 1;
+        }
+
+        if (arr_b[i] == arr_b[i-1]) {
+            longest_b++;
+        } else {
+            longest_b = 1;
+        }
+
+        count_a_map[arr_a[i]] = max(count_a_map[arr_a[i]], longest_a);
+        count_b_map[arr_b[i]] = max(count_b_map[arr_b[i]], longest_b);
+
+
+        unique_elements.insert(arr_a[i]);
+        unique_elements.insert(arr_b[i]);
+    }
+
+
+
+    ll result = 0;
+    for (const int& element : unique_elements) {
+        int count_a = count_a_map[element];
+        int count_b = count_b_map[element];
+        result = max(result, (ll)count_a + (ll)count_b);
+    }
+    cout << result << endl;
+}
+
+int main() {
+    fast_io;
+
+    int t = 1;
+    cin >> t; // uncomment if multiple test cases
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
