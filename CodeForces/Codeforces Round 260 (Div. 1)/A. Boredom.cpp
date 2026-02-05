99a20247dcb91b843f0d6b49523fdefc57ced914
KhaledKammoun <khaledsfax03@gmail.com>
2026-02-05 15:03:39 +0100

    Solve Codeforces problems

--- CodeForces/Codeforces Round 260 (Div. 1)/A. Boredom.cpp
+++ CodeForces/Codeforces Round 260 (Div. 1)/A. Boredom.cpp
@@ -50,69 +50,30 @@ using namespace std;
 #define fvec_char(x,v) for (vector<char>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
 #define fvec_string(x,v) for (vector<string>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
 
-int getResult(vector<int>& unique_elements, unordered_map<int, int>& freq, bool reverseOrder) {
-
-    if (reverseOrder) {
-        reverse(all(unique_elements));
-    }
-
-    int result = 0;
-    f0(i, unique_elements.size()) {
-        
-        int x = unique_elements[i];
-        
-        if (freq[x] == 0) continue; // already used
-
-        int sumX = x * freq[x];
-        bool isThereNext = i + 1 < unique_elements.size() && freq[x + 1] != 0 && unique_elements[i + 1] == x + 1;
-        int sumNext = 0;
-        if (isThereNext) {
-            int y = unique_elements[i + 1];
-            sumNext = y * freq[y];
-            
-        }
-
-        if (sumX >= sumNext || !isThereNext) {
-            result += sumX;
-
-            freq[x] = 0;
-            if (isThereNext) {
-                freq[x + 1] = 0;
-            }
-
-            bool isThereBefore = i - 1 >= 0 && freq[x - 1] != 0 && unique_elements[i - 1] == x - 1;
-            if (isThereBefore) {
-                freq[x - 1] = 0;
-            }
-        }
-    }
-
-    return result;
-
-
-}
-
 void solve() {
     int n; cin >> n;
-    vector<int> arr(n);
+    vector<ll> arr(n);
     read_vector(arr,n);
 
-    unordered_map<int, int> freq;
-    f0(i, n) {
-        freq[arr[i]]++;
+    int max_val = 0;
+    vector<int> cnt(100001,0);
+    f0(i,n) {
+        cnt[arr[i]]++;
+        if (arr[i] > max_val) max_val = arr[i];
     }
 
-    // Sort unique elements
-    vector<int> unique_elements;
-    for (auto& p : freq) {
-        unique_elements.pb(p.first);
+    vector<ll> gain(100001,0);
+    for (int i = 1; i <= max_val; i++) {
+        gain[i] = 1LL * i * cnt[i];
     }
-    sort(all(unique_elements));
 
-    int result = getResult(unique_elements, freq, false);
+    vector<ll> dp(100001,0);
+    if (max_val >= 1) dp[1] = gain[1];
+    for (int i = 2; i <= max_val; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + gain[i]);
+    }
 
-    result+=getResult(unique_elements, freq, true);
-    cout << result << endl;
+    cout << dp[max_val] << endl;
 }
 
 int main() {
