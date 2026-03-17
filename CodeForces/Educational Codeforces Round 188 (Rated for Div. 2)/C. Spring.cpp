0026b9938d000a70678909a791e53430cfc76dc6
KhaledKammoun <khaledsfax03@gmail.com>
2026-03-17 09:34:15 +0100

    Solve problems A, B and C from Educational Codeforces Round 188 (Rated for Div. 2)

---
+++ CodeForces/Educational Codeforces Round 188 (Rated for Div. 2)/C. Spring.cpp
@@ -0,0 +1,95 @@
+// ----------------- Competitive Programming Template -----------------
+// Author: Khaled Kammoun
+// GitHub: https://github.com/KhaledKammoun
+// LinkedIn: https://www.linkedin.com/in/khaled-kammoun-6b1059308/
+// #FREE_PALESTINE
+// #STOP_GENOCIDE_IN_GAZA
+// -------------------------------------------------------------------
+// Date: 2026-03-16
+// -------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define endl '\n'
+#define MOD 1000000007LL
+
+// ----------------- IO & VECTOR MACROS -----------------
+#define read_vector(v,n) for (int i = 0; i < n; i++) cin >> v[i];
+#define print_vector(v) for (size_t i = 0; i < v.size(); i++) { cout << v[i] << ' '; } cout << endl;
+#define print_vector_pair(v) for (size_t i = 0; i < v.size(); i++) { cout << v[i].first << " " << v[i].second << endl; }
+#define print_map_int(m) for (map<int,int>::iterator it = (m).begin(); it != (m).end(); ++it) cout << it->first << ": " << it->second << endl;
+#define print_map_ll(m) for (map<long long,long long>::iterator it = (m).begin(); it != (m).end(); ++it) cout << it->first << ": " << it->second << endl;
+
+// ----------------- FOR LOOPS -----------------
+#define f(i,x,n) for (int i = x; i < n; i++)
+#define f0(i,n) for (int i = 0; i < n; i++)
+#define f1(i,n) for (int i = 1; i < n; i++)
+
+// ----------------- MAP ITERATORS -----------------
+#define fmap_int(k,v,m) for (map<int,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_ll(k,v,m) for (map<long long,long long>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_char(k,v,m) for (map<char,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_string(k,v,m) for (map<string,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+#define fmap_pair(k,v,m) for (map<pair<int,int>,int>::iterator it = (m).begin(); it != (m).end() && ((k=it->first)&&(v=it->second),1); ++it)
+
+// ----------------- SET ITERATORS -----------------
+#define fmap_set(s,m) for (set<int>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+#define fmap_set_ll(s,m) for (set<long long>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+#define fmap_set_char(s,m) for (set<char>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+#define fmap_set_string(s,m) for (set<string>::iterator it = (m).begin(); it != (m).end() && ((s=*it),1); ++it)
+
+// ----------------- VECTOR ITERATORS -----------------
+#define fvec_int(x,v) for (vector<int>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+#define fvec_ll(x,v) for (vector<long long>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+#define fvec_char(x,v) for (vector<char>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+#define fvec_string(x,v) for (vector<string>::iterator it = (v).begin(); it != (v).end() && ((x=*it),1); ++it)
+
+long long lcm(long long a, long long b) {
+    return a / __gcd(a,b) * b;
+}
+
+void solve() {
+    // int n; cin >> n;
+    // vector<int> arr(n);
+    // read_vector(arr,n);
+    long long a, b, c , m; 
+    cin >> a >> b >> c >> m;
+    
+    long long three = m / lcm(a, lcm(b, c)) ;
+    
+    long long aliceB = m / lcm(a, b) - three ;
+    long long alieceC = m / lcm(a, c) - three ;
+
+
+    long long bobC = m / lcm(b, c) - three ;
+
+    long long alice = m / a ;
+    long long bob = m / b ;
+    long long charlie = m / c ;
+
+    long long resultA = three * 2 + aliceB * 3 + alieceC * 3 + (alice - three - aliceB - alieceC) * 6 ;
+    long long resultB = three * 2 + aliceB * 3 + bobC * 3 + (bob - three - aliceB - bobC) * 6 ;
+    long long resultC = three * 2 + alieceC * 3 + bobC * 3 + (charlie - three - alieceC - bobC) * 6 ;
+
+    cout << resultA << " " << resultB << " " << resultC << endl;
+}
+
+
+int main() {
+    fast_io;
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
