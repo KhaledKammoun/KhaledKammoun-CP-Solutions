74369c905423d65da9f3dc69f69d638a1610b3f7
KhaledKammoun <khaledsfax03@gmail.com>
2026-04-04 17:59:53 +0100

    Solve problems A, B and C from Codeforces Round 1090 (Div. 4)

---
+++ CodeForces/Codeforces Round 1090 (Div. 4)/D. The 67th OEIS Problem.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+vector<ll> primes;
+void sieve(int lim) {
+    vector<bool> is_prime(lim+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= lim; i++)
+        if (is_prime[i]) {
+            primes.push_back(i);
+            for (ll j = (ll)i*i; j <= lim; j += i)
+                is_prime[j] = false;
+        }
+}
+
+void solve() {
+    int n; cin >> n;
+
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        a[i] = primes[i] * primes[i+1];
+    
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " \n"[i==n-1];
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    sieve(200000); 
+    int t; cin >> t;
+    while(t--) solve();
+}
